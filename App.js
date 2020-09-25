7ca09dfe633ec1e0239eb8209db25486d2dbb51b
sihyung-ahn <sihyungahn817@gmail.com>
2020-09-25 04:27:34 +0000

    f

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ async function cameraStoragePermission(){
 	const { status } = await Permissions.getAsync(
 		Permission.CAMERA,
 		Permissions.READ_EXTERNAL_STORAGE
-		);
+	);
 		if (status !=='grant'){
 		alert('camera/album need authorization to use');
 		}
-	}
+}
+
 componentDidMount(){
 	this.cameraStoragePermission;
 }
