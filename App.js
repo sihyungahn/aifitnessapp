58181a068672ed503d91fdca89e27723c104fd21
sihyung-ahn <sihyungahn817@gmail.com>
2020-09-25 04:02:22 +0000

    G

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
+import * as Permissions from 'expo-permissions';
+
+async function cameraStoragePermission(){
+	const { status } = await Permissions.getAsync(
+		Permission.CAMERA,
+		Permissions.READ_EXTERNAL_STORAGE
+		);
+		if (status !=='grant'){
+		alert('camera/album need authorization to use');
+		}
+	}
+componentDidMount(){
+	this.cameraStoragePermission;
+}
+
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
 
 export default class App extends React.Component {
   render() {
-    return <WebView source={{ uri: 'https://aifitnesstrainer.net/' }} style={{ marginTop: 20 }} />;
+    return <WebView source={{ uri: 'https://aifitnesstrainer.net' }} style={{ marginTop: 20 }} />;
   }
 }
