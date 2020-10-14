660dd8ad143717e20823627a994a07c99527a02e
sihyung-ahn <sihyungahn817@gmail.com>
2020-10-14 03:34:37 +0000

    g

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ import {WebView} from 'react-native-webview';
 
 export default class App extends React.Component {
 	render() {
-		return <WebView source ={{ url: 'https://aifitnesstrainer.net/'}} style={{marginTop: 20}} />;
+		return <WebView source ={{ uri: 'https://aifitnesstrainer.net/'}} style={{marginTop: 20}} />;
 	}
 }
